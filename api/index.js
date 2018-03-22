e5023829a73f703f47f8569bf7a2cc9aad22119b
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-03-22 19:08:44 +0200

    Update columns icon size

--- api/index.js
+++ api/index.js
@@ -7,10 +7,10 @@ const projects = {
 };
 
 app.get('/api/elements', (req, res) => {
-  setTimeout(() => res.send(projects.data), 3000);
+  setTimeout(() => res.send(projects.data), 2000);
 });
 app.get('/api/elements/grid', (req, res) => {
-  setTimeout(() => res.send(projects.grid), 6000);
+  setTimeout(() => res.send(projects.grid), 3000);
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
