6bf35fac8ec06266351696cc631b5d3c93690429
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-03-27 19:01:00 +0300

    Remove extra code

--- src/components/rest-grid/rest-grid.service.ts
+++ src/components/rest-grid/rest-grid.service.ts
@@ -1,52 +1,14 @@
 import {Injectable} from '@angular/core';
-import {
-  ColumnInterface,
-  GridOptionsInterface
-} from './options/grid-options.interface';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
 
 @Injectable()
 export class RestGridService {
   filters: Map<String, Object> = new Map<String, Object>();
   sorters: Map<String, Array<String>> = new Map<String, Array<String>>();
 
-  gridOptions: Observable<GridOptionsInterface> = null;
-  elements: Observable<ColumnInterface[]> = null;
-
-  url: '/api/elements';
-
-  constructor(private http: HttpClient) {
+  constructor() {
     this.filters.set('filters', []);
     this.sorters.set('asc', []);
     this.sorters.set('desc', []);
-  }
-
-  getElements(): Observable<ColumnInterface[]> {
-    if (!this.elements) {
-      this.elements = this.http.get(`${this.url}`).map((response: ColumnInterface[]) => {
-        const elements: ColumnInterface[] = [];
-
-        response.forEach((e: ColumnInterface) => {
-          elements.push(e);
-        });
-
-        return elements;
-      });
-    }
-
-    return this.elements;
-  }
-
-  getGridOptions(): Observable<GridOptionsInterface> {
-    if (!this.gridOptions) {
-      this.gridOptions = this.http.get(`${this.url}/options`).map((response: GridOptionsInterface) => {
-        return response;
-      });
-    }
-
-    return this.gridOptions;
   }
 
   // addFilter('column1', '>', 2)
