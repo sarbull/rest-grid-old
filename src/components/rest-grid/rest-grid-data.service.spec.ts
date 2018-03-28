08edbe5fceda1b1293d554fdabb6ae495611879d
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-03-28 22:58:53 +0300

    Add rest-grid-data.service specs and add code-coverage flag

--- src/components/rest-grid/rest-grid-data.service.spec.ts
+++ src/components/rest-grid/rest-grid-data.service.spec.ts
@@ -1,21 +1,34 @@
 import {RestGridDataService} from './rest-grid-data.service';
-import {TestBed} from '@angular/core/testing';
-import {HttpClientModule} from '@angular/common/http';
+import {
+  TestBed,
+  getTestBed
+} from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
 describe('RestGridDataService', () => {
-  let service;
+  let service: RestGridDataService;
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        HttpClientModule
+        HttpClientTestingModule
       ],
       providers: [
         RestGridDataService
       ]
     });
+    injector = getTestBed();
+    service = injector.get(RestGridDataService);
+    httpMock = injector.get(HttpTestingController);
+  });
 
-    service = TestBed.get(RestGridDataService);
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should set an url', () => {
@@ -38,5 +51,91 @@ describe('RestGridDataService', () => {
     service.setQueryParam('query2', '');
 
     expect(service.getUrl()).toEqual('/api/test');
+  });
+
+  it('should return an Observable<GridOptionsInterface> on get grid options', () => {
+    const gridOptions = {
+      columns: [
+        {
+          name: 'position',
+          type: 'number',
+          filter: false,
+          sort: false
+        }
+      ]
+    };
+
+    service.setUrl('/api/test');
+
+    service.getGridOptions().subscribe(data => {
+      expect(gridOptions.columns.length).toBe(1);
+      expect(data).toEqual(gridOptions);
+    });
+
+    const req = httpMock.expectOne(`${service.getUrl()}/options`);
+    expect(req.request.method).toBe('GET');
+    req.flush(gridOptions);
+  });
+
+  it('should return an Observable<DataModelInterface> on get elements', () => {
+    const elements = {
+      items: [
+        {
+          position: 1,
+          name: 'Hydrogen',
+          weight: 1.0079,
+          symbol: 'H'
+        }
+      ],
+      totalCount: 1,
+      itemsPerPage: 10,
+      currentPage: 1
+    };
+
+    service.setUrl('/api/test');
+
+    service.getElements().subscribe(data => {
+      expect(elements.items.length).toBe(1);
+      expect(elements.totalCount).toBe(1);
+      expect(elements.itemsPerPage).toBe(10);
+      expect(elements.currentPage).toBe(1);
+
+      expect(data).toEqual(elements);
+    });
+
+    const req = httpMock.expectOne(service.getUrl());
+    expect(req.request.method).toBe('GET');
+    req.flush(elements);
+  });
+
+  it('should return an Observable<DataModelInterface> on query elements', () => {
+    const elements = {
+      items: [
+        {
+          position: 1,
+          name: 'Hydrogen',
+          weight: 1.0079,
+          symbol: 'H'
+        }
+      ],
+      totalCount: 1,
+      itemsPerPage: 10,
+      currentPage: 1
+    };
+
+    service.setUrl('/api/test');
+
+    service.queryElements(1, 10).subscribe(data => {
+      expect(elements.items.length).toBe(1);
+      expect(elements.totalCount).toBe(1);
+      expect(elements.itemsPerPage).toBe(10);
+      expect(elements.currentPage).toBe(1);
+
+      expect(data).toEqual(elements);
+    });
+
+    const req = httpMock.expectOne(service.getUrl());
+    expect(req.request.method).toBe('GET');
+    req.flush(elements);
   });
 });
