04f6ebc3d946f39fafc1feeaa8b522727e27b413
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-04-02 19:54:12 +0300

    Merge sorter observable with paginator, beta

--- src/components/rest-grid/rest-grid.component.ts
+++ src/components/rest-grid/rest-grid.component.ts
@@ -15,6 +15,7 @@ import {startWith} from 'rxjs/operators/startWith';
 import {switchMap} from 'rxjs/operators/switchMap';
 import {RestGridDataService} from './rest-grid-data.service';
 import {DataModelInterface, GridOptionsInterface} from './options/grid-options.interface';
+import {Subject} from 'rxjs/Subject';
 
 @Component({
   selector: 'rg-rest-grid',
@@ -28,44 +29,92 @@ export class RestGridComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   dataSource: MatTableDataSource<DataModelInterface> = new MatTableDataSource<DataModelInterface>();
 
+  url: Subject<string> = new Subject<string>();
+
 
   constructor(private restGridDataService: RestGridDataService) {
   }
 
   ngOnInit() {
     this.restGridDataService.setUrl(this.endpoint);
-    const options = this.restGridDataService.getGridOptions();
 
-    const elements = combineLatest(
-      this.paginator.page
+    const merged = merge(
+      this.paginator.page,
+      this.url
     ).pipe(
       startWith({}),
       switchMap(() => {
         this.isLoadingResults = true;
 
-        return this.restGridDataService.queryElements(
-          this.paginator.pageIndex,
-          this.paginator.pageSize
-        );
+        this.restGridDataService.setQueryParam('currentPage', this.paginator.pageIndex + 1);
+        this.restGridDataService.setQueryParam('itemsPerPage', this.paginator.pageSize);
+
+        return this.restGridDataService.getElements();
       }),
       map(data => {
+
         return data;
       })
     );
 
+    const options = this.restGridDataService.getGridOptions();
+
+    // const elements = this.restGridDataService.getElements();
+
     combineLatest(
       options,
-      elements
-    ).subscribe((data) => {
-      this.displayedColumns = data[0].columns.map((e) => e.name);
-      this.dataSource.data = data[1].items;
-      this.resultsLength = data[1].totalCount;
+      merged
+      // elements
+    ).subscribe(data => {
+      const [optionsData, mergedData] = data;
+
+      console.log(data);
+
+      this.displayedColumns = optionsData.columns.map((e) => e.name);
+      this.dataSource.data = mergedData.items;
+      this.resultsLength = mergedData.totalCount;
       this.isLoadingResults = false;
     });
+
+
+
+
+
+
+    // const options = this.restGridDataService.getGridOptions();
+    //
+    // const elements = merge(
+    //   this.paginator.page
+    // ).pipe(
+    //   startWith({}),
+    //   switchMap(() => {
+    //     this.isLoadingResults = true;
+    //
+    //     return this.restGridDataService.queryElements(
+    //       this.paginator.pageIndex,
+    //       this.paginator.pageSize
+    //     );
+    //   }),
+    //   map(data => {
+    //     return data;
+    //   })
+    // );
+    //
+    // combineLatest(
+    //   options,
+    //   elements
+    // ).subscribe((data) => {
+    //   this.displayedColumns = data[0].columns.map((e) => e.name);
+    //   this.dataSource.data = data[1].items;
+    //   this.resultsLength = data[1].totalCount;
+    //   this.isLoadingResults = false;
+    // });
   }
 
   doSort(column: string): void {
     this.restGridDataService.doSort(column);
+
+    this.url.next();
   }
 
   isInAsc(column: string): boolean {
